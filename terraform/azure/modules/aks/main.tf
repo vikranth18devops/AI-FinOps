f35aac410f5853b35add4ccfce69f99994b7dd5b
Vikranth Sunkarpally <50102593+vikranth18devops@users.noreply.github.com>
2026-08-15 12:46:12 +0530

    Configure non-overlapping service_cidr (10.1.0.0/16) and dns_service_ip (10.1.0.10) for AKS

--- terraform/azure/modules/aks/main.tf
+++ terraform/azure/modules/aks/main.tf
@@ -28,6 +28,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
+    service_cidr      = "10.1.0.0/16"
+    dns_service_ip    = "10.1.0.10"
   }
 
   tags = {
