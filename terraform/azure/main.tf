5084b85140d29894c64310f71cfe1e90766b76cb
Vikranth Sunkarpally <50102593+vikranth18devops@users.noreply.github.com>
2026-08-15 12:43:24 +0530

    Upgrade azurerm provider version to stable v3.117.1 to fix AKS API version error

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80.0"
+      version = ">= 3.90.0, < 4.0.0"
     }
   }
 
@@ -17,7 +17,6 @@ terraform {
 }
 
 provider "azurerm" {
-  skip_provider_registration = true
   features {}
 }
 
