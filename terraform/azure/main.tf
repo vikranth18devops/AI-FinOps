c5af77ec2ee2a65b6e31b4642f68d8a8b056ff0a
Vikranth Sunkarpally <50102593+vikranth18devops@users.noreply.github.com>
2026-08-16 00:46:57 +0530

    chore(terraform): update azure remote state storage account name

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -10,7 +10,7 @@ terraform {
   # Azure Remote Backend State Storage
   backend "azurerm" {
     resource_group_name  = "finops-global-rg"
-    storage_account_name = "tfstatefinops7261"
+    storage_account_name = "tfstatefinops23512"
     container_name       = "tfstate"
     key                  = "azure.aks.tfstate"
   }
